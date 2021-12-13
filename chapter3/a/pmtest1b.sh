83a8603c93b09257a5fabd2810c1798fec93707a
pn20120162 <103@qq.com>
2021-12-13 19:29:13 +0800

    os

---
+++ chapter3/a/pmtest1b.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# freedos官网下载地址 https://bochs.sourceforge.io/diskimages.html
+# 要先在freedos中执行   format b:
+nasm pmtest1b.asm -o pmtest1b.com
+sudo mount -o loop a.img /mnt/floppy/
+sudo cp pmtest1b.com /mnt/floppy/
+sudo umount /mnt/floppy
